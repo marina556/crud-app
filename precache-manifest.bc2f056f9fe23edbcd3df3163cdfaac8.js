007302109079449b86111838b10a48f3c392d1f8
marina556 <marinamakram556@gmail.com>
2020-06-25 18:17:43 +0200

    Updates

---
+++ precache-manifest.bc2f056f9fe23edbcd3df3163cdfaac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c27f0e0e8f51f55ac551723c423d087",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "e43dfe0f89b8a70e5b90",
+    "url": "/crud-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b830ddaab72b54f327db",
+    "url": "/crud-app/static/css/main.48e69742.chunk.css"
+  },
+  {
+    "revision": "e43dfe0f89b8a70e5b90",
+    "url": "/crud-app/static/js/2.56f8e88b.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/crud-app/static/js/2.56f8e88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b830ddaab72b54f327db",
+    "url": "/crud-app/static/js/main.b040abed.chunk.js"
+  },
+  {
+    "revision": "6400a072754635706ea1",
+    "url": "/crud-app/static/js/runtime-main.b590a2b6.js"
+  }
+]);
